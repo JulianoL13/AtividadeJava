16cea4f9caf0e988f80de57c74c57ff5ca097610
JulianoL13 <125844980+JulianoL13@users.noreply.github.com>
2024-09-09 19:59:14 -0300

    Update Funcionario.java

--- atv2x/Funcionario.java
+++ atv2x/Funcionario.java
@@ -1,12 +1,23 @@
 public class Funcionario {
-  private String nome;
-  private double salarioBruto;
-  private double taxad;
+    private String nome;
+    private double salarioBruto;
+    private double taxa;
 
-  public Funcionario(String nome, double salarioBruto, double taxad){
-    this.nome = nome;
-    this.salarioBruto = salarioBruto;
-    this.taxad = taxad;
-      
-  }
-  }
+    public Funcionario(String nome, double salarioBruto, double taxa) {
+        this.nome = nome;
+        this.salarioBruto = salarioBruto;
+        this.taxa = taxa;
+    }
+
+    public double calcularSalario(double porcentagem) {
+        double salarioLiquido = this.salarioBruto - this.taxa;
+        double acrescimo = (this.salarioBruto * porcentagem) / 100;
+        return salarioLiquido + acrescimo;
+    }
+
+    public void imprimir(double porcentagem) {
+        double salarioAtualizado = calcularSalario(porcentagem); 
+        System.out.println("O nome é: " + this.nome);
+        System.out.println("Salario atualizado: " + salarioAtualizado);
+    }
+}
